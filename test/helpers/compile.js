8a5b7e3aa06ee0febe8f611fd1774a7c25b6ecac
Kevin Montag <kmontag@cs.stanford.edu>
2017-11-10 16:41:54 +0100

    fix(init): more use strict

--- test/helpers/compile.js
+++ test/helpers/compile.js
@@ -1,3 +1,5 @@
+"use strict";
+
 const MemoryFS = require('memory-fs');
 const path = require('path');
 const webpack = require('webpack');
--- test/index.test.js
+++ test/index.test.js
@@ -1,3 +1,5 @@
+"use strict";
+
 const assert = require('chai').assert;
 const path = require('path');
 
