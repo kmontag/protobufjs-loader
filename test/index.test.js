1af18121188fec30335708036111bb0a9a2e7a8a
Kevin Montag <kmontag@cs.stanford.edu>
2022-07-18 01:04:53 +0200

    test: remove `uglify-loader` dependency, uglify manually in tests

--- test/index.test.js
+++ test/index.test.js
@@ -2,8 +2,35 @@
 
 const assert = require('chai').assert;
 const path = require('path');
+const UglifyJS = require('uglify-js');
 
 const compile = require('./helpers/compile');
+
+/**
+ * Minifies the given JS string using ugilfy-js, so we can
+ * consistently compare generated outputs using relatively compact
+ * strings.
+ */
+function minify(contents) {
+  const result = UglifyJS.minify(contents, {
+    compress: {
+      // This avoids some larger structural changes in the minified
+      // code during compression.
+      inline: false,
+    },
+
+    // Don't mangle function/variable names.
+    mangle: false,
+
+    // Don't remove names of functions which aren't referenced by name
+    // somewhere.
+    keep_fnames: true,
+  });
+  if (result.error) {
+    throw new Error(result.error);
+  }
+  return result.code;
+}
 
 before(function(done) {
   // The first time the compiler gets run (e.g. in a CI environment),
@@ -24,8 +51,8 @@ describe('with JSON / reflection', function() {
   });
   it('should compile to a JSON representation', function(done) {
     compile('basic', this.opts).then(function(inspect) {
-      let contents = inspect.arguments[0];
-      let innerString = 'addJSON({foo:{nested:{Bar:{fields:{baz:{type:"string",id:1}}}}}})})'
+      let contents = minify(inspect.arguments[0]);
+      let innerString = 'addJSON({foo:{nested:{Bar:{fields:{baz:{type:"string",id:1}}}}}})})';
       assert.include(contents, innerString);
       done();
     });
@@ -35,7 +62,7 @@ describe('with JSON / reflection', function() {
 describe('with static code', function() {
   it('should compile static code by default', function(done) {
     compile('basic').then(function(inspect) {
-      let contents = inspect.arguments[0];
+      let contents = minify(inspect.arguments[0]);
       assert.include(contents, 'foo.Bar=function(){');
       done();
     });
@@ -43,7 +70,7 @@ describe('with static code', function() {
 
   it('should compile static code when the option is set explicitly', function(done) {
     compile('basic', {json: false}).then(function(inspect) {
-      let contents = inspect.arguments[0];
+      let contents = minify(inspect.arguments[0]);
       assert.include(contents, 'foo.Bar=function(){');
       done();
     });
@@ -53,9 +80,10 @@ describe('with static code', function() {
 describe('with command line options', function() {
   it('should pass command line options to the pbjs call', function(done) {
     compile('basic', {pbjsArgs: ['--no-encode']}).then(function(inspect) {
-      let contents = inspect.arguments[0];
+      let contents = minify(inspect.arguments[0]);
+
       // Sanity check
-      let innerString = 'Bar.decode=function(reader,length)';
+      let innerString = 'Bar.decode=function decode(reader,length)';
       assert.include(contents, innerString);
 
       assert.notInclude(contents, 'encode');
@@ -78,7 +106,7 @@ describe('with imports', function() {
         modules: ['node_modules', path.resolve(__dirname, 'fixtures')],
       }
     }).then(function(inspect) {
-      let contents = inspect.arguments[0];
+      let contents = minify(inspect.arguments[0]);
       assert.include(contents, innerString);
       done();
     });
@@ -90,7 +118,7 @@ describe('with imports', function() {
       json: true,
       paths: [path.resolve(__dirname, 'fixtures')],
     }).then(function(inspect) {
-      let contents = inspect.arguments[0];
+      let contents = minify(inspect.arguments[0]);
       assert.include(contents, innerString);
       done();
     });
