226c8f7aeed884be7cd924e741640d1bbd4946c6
Kevin Montag <kmontag@cs.stanford.edu>
2022-07-18 02:18:57 +0200

    Remove stray import

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 'use strict';
 
 const fs = require('fs');
-const path = require('path');
 const pbjs = require('protobufjs/cli').pbjs;
 const protobuf = require('protobufjs');
 const tmp = require('tmp-promise');
