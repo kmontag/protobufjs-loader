144bfd043c7d6762c35fe66a3f43b083044a5106
Kevin Montag <kmontag@cs.stanford.edu>
2023-05-20 04:23:34 +0200

    build: stricter types for webpack configuration in test helper

--- test/helpers/compile.js
+++ test/helpers/compile.js
@@ -22,8 +22,6 @@ const fixturePath = path.resolve(__dirname, '..', 'fixtures');
 // The config object needs to look slightly different depending on the
 // version of webpack that we're testing with.
 const isWebpack4Plus = 'version' in webpack;
-const isWebpack4 =
-  isWebpack4Plus && (webpack.version || '').substring(0, 2) === '4.';
 const isWebpack5 =
   isWebpack4Plus && (webpack.version || '').substring(0, 2) === '5.';
 
@@ -36,6 +34,15 @@ const isWebpack5 =
  * @typedef {{ arguments: [string], context: import('../../index').LoaderContext, options: never }} InspectLoaderResult
  */
 
+/**
+ * Helper types for each webpack config schema.
+ *
+ * @typedef { import('webpack2').Configuration } Webpack2Config
+ * @typedef { import('webpack3').Configuration } Webpack3Config
+ * @typedef { import('webpack4').Configuration } Webpack4Config
+ * @typedef { import('webpack').Configuration } Webpack5Config
+ */
+
 /** @type { (fixture: string, loaderOpts?: object, webpackOpts?: object) => Promise<{ inspect: InspectLoaderResult }> } */
 module.exports = function compile(fixture, loaderOpts, webpackOpts) {
   return new Promise((resolve, reject) => {
@@ -43,62 +50,62 @@ module.exports = function compile(fixture, loaderOpts, webpackOpts) {
     let inspect;
 
     /**
-     * @type { ReturnType<typeof webpack> }
+     * @type { Webpack2Config | Webpack3Config | Webpack4Config | Webpack5Config }
      */
-    const compiler =
-      // The function signatures for different webpack versions
-      // aren't compatible, so the compiler thinks this call is
-      // impossible. Note we should still get a runtime error if
-      // the configuration schema isn't valid.
-      //
-      // @ts-ignore
-      webpack(
-        // @ts-ignore
-        {
-          entry: path.resolve(fixturePath, `${fixture}.proto`),
-          output: {
-            path: '/',
-            filename: 'compiled.js',
-            // By default, webpack@4 uses a hash function (md4) which
-            // is not supported by the Node 17+ SSL provider. Set it
-            // explicitly to avoid a compilation error unrelated to
-            // protobufjs. See
-            // https://stackoverflow.com/a/73465262/13264260.
-            ...(isWebpack4 ? { hashFunction: 'md5' } : {}),
-          },
-          module: {
-            rules: [
+    const config = {
+      entry: path.resolve(fixturePath, `${fixture}.proto`),
+      output: {
+        path: '/',
+        filename: 'compiled.js',
+        // By default, webpack@4 uses a hash function (md4) which
+        // is not supported by the Node 17+ SSL provider. Set it
+        // explicitly to avoid a compilation error unrelated to
+        // protobufjs. See
+        // https://stackoverflow.com/a/73465262/13264260.
+        ...(isWebpack4Plus ? { hashFunction: 'md5' } : {}),
+      },
+      module: {
+        rules: [
+          {
+            test: /\.proto$/,
+            use: [
               {
-                test: /\.proto$/,
-                use: [
-                  {
-                    loader: 'inspect-loader',
-                    options: {
-                      /** @type { (inspect: InspectLoaderResult) => any } */
-                      callback(callbackInspect) {
-                        inspect = callbackInspect;
-                      },
-                    },
+                loader: 'inspect-loader',
+                options: {
+                  /** @type { (inspect: InspectLoaderResult) => any } */
+                  callback(callbackInspect) {
+                    inspect = callbackInspect;
                   },
-                  {
-                    loader: path.resolve(__dirname, '..', '..', 'index.js'),
-                    options: loaderOpts,
-                  },
-                ],
+                },
+              },
+              {
+                loader: path.resolve(__dirname, '..', '..', 'index.js'),
+                options: loaderOpts,
               },
             ],
           },
-          // webpack@4 adds the `mode` configuration option, which adds some
-          // additional config defaults that we want to avoid for
-          // consistency.
-          ...(isWebpack4Plus ? { mode: 'none' } : {}),
-          // Make sure to test webpack@5 without backwards-compatibility
-          // enabled. See
-          // https://webpack.js.org/configuration/experiments/#experimentsbackcompat.
-          ...(isWebpack5 ? { experiments: { backCompat: false } } : {}),
-          ...webpackOpts,
-        }
-      );
+        ],
+      },
+      // webpack@4 adds the `mode` configuration option, which adds some
+      // additional config defaults that we want to avoid for
+      // consistency.
+      ...(isWebpack4Plus ? { mode: 'none' } : {}),
+      // Make sure to test webpack@5 without backwards-compatibility
+      // enabled. See
+      // https://webpack.js.org/configuration/experiments/#experimentsbackcompat.
+      ...(isWebpack5 ? { experiments: { backCompat: false } } : {}),
+      ...webpackOpts,
+    };
+
+    /**
+     * @type { ReturnType<typeof webpack> }
+     */
+    const compiler =
+      // The function signatures for different webpack versions aren't
+      // compatible, so the compiler thinks this call is impossible.
+      //
+      // @ts-ignore
+      webpack(config);
 
     const fs = new MemoryFS();
 
@@ -139,6 +146,7 @@ module.exports = function compile(fixture, loaderOpts, webpackOpts) {
 
         return undefined;
       })();
+
       if (problem) {
         reject(problem);
       } else {
