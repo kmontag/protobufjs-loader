94491ad7ab4c0866d14c454be0d71ea108349340
Kevin Montag <kmontag@cs.stanford.edu>
2025-01-07 13:36:39 -0800

    refactor: switch to async/await syntax (#47)
    Refactor manual promise chaining to `async`/`await` syntax for
    readability and maintainability. This should be a completely transparent
    change, except that errors thrown by the loader may look slightly
    different than before, as they're no longer being constructed manually
    within `catch` blocks/callbacks.
    
    Note that per https://node.green/#ES2017-features-async-functions,
    `async`/`await` syntax has been supported natively since Node 7.10,
    which is below this package's specified minimum version (12), as well as
    the specified minimum versions of both webpack and protobufjs.
    Therefore, this isn't being treated as a breaking change.

--- index.js
+++ index.js
@@ -69,67 +69,221 @@ const schema = {
  * @typedef { import('webpack').LoaderContext<Partial<LoaderOptions>> } LoaderContext
  */
 
-/** @type { (resourcePath: string, pbtsOptions: true | PbtsOptions, compiledContent: string, callback: NonNullable<ReturnType<LoaderContext['async']>>) => any } */
-const execPbts = (resourcePath, pbtsOptions, compiledContent, callback) => {
-  try {
-    /** @type PbtsOptions */
-    const normalizedOptions = {
-      args: [],
-      output: null,
-      ...(pbtsOptions === true ? {} : pbtsOptions),
-    };
+/** @type { (resourcePath: string, pbtsOptions: true | PbtsOptions, compiledContent: string | undefined) => Promise<void> } */
+const execPbts = async (resourcePath, pbtsOptions, compiledContent) => {
+  /** @type PbtsOptions */
+  const normalizedOptions = {
+    args: [],
+    output: null,
+    ...(pbtsOptions === true ? {} : pbtsOptions),
+  };
 
-    // pbts CLI only supports streaming from stdin without a lot of
-    // duplicated logic, so we need to use a tmp file. :(
-    /** @type Promise<string> */
-    const compiledFilenamePromise = new Promise((resolve, reject) => {
-      tmp.file({ postfix: '.js' }, (err, compiledFilename) => {
-        if (err) {
-          reject(err);
-        } else {
-          resolve(compiledFilename);
-        }
-      });
-    }).then(
-      (compiledFilename) =>
-        new Promise((resolve, reject) => {
-          fs.writeFile(compiledFilename, compiledContent, (err) => {
-            if (err) {
-              reject(err);
-            } else {
-              resolve(compiledFilename);
-            }
-          });
-        })
-    );
-    /** @type { (resourcePath: string) => string | Promise<string> } */
-    const output =
-      normalizedOptions.output === null
-        ? (r) => `${r}.d.ts`
-        : normalizedOptions.output;
-    const declarationFilenamePromise = Promise.resolve(output(resourcePath));
+  /**
+   * Immediately run the function to get the typescript output path. If
+   * the function is asynchronous, it will run in the background while
+   * we kick off other async operations.
+   *
+   * @type { (resourcePath: string) => string | Promise<string> }
+   */
+  const output =
+    normalizedOptions.output === null
+      ? (r) => `${r}.d.ts`
+      : normalizedOptions.output;
+  const declarationFilenamePromise = Promise.resolve(output(resourcePath));
 
-    Promise.all([compiledFilenamePromise, declarationFilenamePromise])
-      .then(([compiledFilename, declarationFilename]) => {
-        const pbtsArgs = ['-o', declarationFilename]
-          .concat(normalizedOptions.args)
-          .concat([compiledFilename]);
-        pbts.main(pbtsArgs, (err) => {
-          callback(err, compiledContent);
-        });
-      })
-      .catch((err) => {
-        callback(err, undefined);
-      });
-  } catch (err) {
-    callback(err instanceof Error ? err : new Error(`${err}`), undefined);
-  }
+  // pbts CLI only supports streaming from stdin without a lot of
+  // duplicated logic, so we need to use a tmp file. :(
+  const compiledFilename = await new Promise((resolve, reject) => {
+    tmp.file({ postfix: '.js' }, (err, result) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(result);
+      }
+    });
+  });
+
+  // Write the compiled JS content to the tmp file.
+  await new Promise((resolve, reject) => {
+    fs.writeFile(compiledFilename, compiledContent || '', (err) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(compiledFilename);
+      }
+    });
+  });
+
+  const declarationFilename = await declarationFilenamePromise;
+  const pbtsArgs = ['-o', declarationFilename]
+    .concat(normalizedOptions.args)
+    .concat([compiledFilename]);
+
+  /** @type { Promise<void> } */
+  const pbtsPromise = new Promise((resolve, reject) => {
+    pbts.main(pbtsArgs, (err) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve();
+      }
+    });
+  });
+  await pbtsPromise;
 };
 
-/** @type { (this: LoaderContext, source: string) => any } */
+/**
+ * Main loader invocation. Return the pbjs-transformed content of a
+ * protobuf source, and write typescript declarations if appropriate.
+ *
+ * @type { (source: string, context: LoaderContext) => Promise<string | undefined> }
+ */
+const loadSource = async (source, context) => {
+  const defaultPaths = (() => {
+    // eslint-disable-next-line no-underscore-dangle
+    if (context._compiler) {
+      // The `_compiler` property is deprecated, but still works as
+      // of webpack@5.
+      //
+      // eslint-disable-next-line no-underscore-dangle
+      return (context._compiler.options.resolve || {}).modules;
+    }
+
+    return undefined;
+  })();
+
+  /** @type LoaderOptions */
+  const options = {
+    target: TARGET_STATIC_MODULE,
+    // Default to the module search paths given to the compiler.
+    paths: defaultPaths || [],
+    pbjsArgs: [],
+    pbts: false,
+    ...context.getOptions(),
+  };
+  validateOptions(schema, options, { name: 'protobufjs-loader' });
+
+  /**
+   * Get a tmp file location and write the file content.
+   *
+   * @type { string }
+   */
+  const filename = await new Promise((resolve, reject) => {
+    tmp.file((err, result) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(result);
+      }
+    });
+  });
+
+  await new Promise((resolve, reject) => {
+    fs.writeFile(filename, source, (err) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(filename);
+      }
+    });
+  });
+
+  const { paths } = options;
+
+  /**
+   * Adapted from the import resolution setup in
+   * https://github.com/dcodeIO/protobuf.js/blob/master/cli/pbjs.js.
+   *
+   * In addition to the main pbjs invocation, run a manual compilation
+   * pass which resolves imports using the provided include paths, and
+   * mark all visited imports as dependencies of the current resource.
+   *
+   * @type { Promise<protobuf.Root> }
+   */
+  const loadDependencies = new Promise((resolve, reject) => {
+    const root = new protobuf.Root();
+
+    // Set up the resolver which will mark dependencies as it goes.
+    root.resolvePath = (origin, target) => {
+      const normOrigin = protobuf.util.path.normalize(origin);
+      const normTarget = protobuf.util.path.normalize(target);
+
+      let resolved = protobuf.util.path.resolve(normOrigin, normTarget, true);
+      const idx = resolved.lastIndexOf('google/protobuf/');
+      if (idx > -1) {
+        const altname = resolved.substring(idx);
+        if (altname in protobuf.common) {
+          resolved = altname;
+        }
+      }
+
+      if (fs.existsSync(resolved)) {
+        // Don't add a dependency on the temp file
+        if (resolved !== protobuf.util.path.normalize(filename)) {
+          context.addDependency(resolved);
+        }
+        return resolved;
+      }
+
+      for (let i = 0; i < paths.length; i += 1) {
+        const iresolved = protobuf.util.path.resolve(`${paths[i]}/`, target);
+        if (fs.existsSync(iresolved)) {
+          context.addDependency(iresolved);
+          return iresolved;
+        }
+      }
+
+      return null;
+    };
+
+    // Perform the actual parsing/dependency resolution, and resolve
+    // when finished, i.e. after all dependencies have been visited
+    // and marked.
+    protobuf.load(filename, root, (err, result) => {
+      if (err || result === undefined) {
+        reject(err);
+      } else {
+        resolve(result);
+      }
+    });
+  });
+
+  /** @type { string[] } */
+  let args = ['-t', options.target];
+  paths.forEach((path) => {
+    args = args.concat(['-p', path]);
+  });
+  args = args.concat(options.pbjsArgs).concat([filename]);
+
+  /**
+   * Run the pbjs compiler and get the compiled content.
+   *
+   * @type { string | undefined }
+   */
+  const compiledContent = await new Promise((resolve, reject) => {
+    pbjs.main(args, (err, result) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(result);
+      }
+    });
+  });
+
+  // If appropriate, run the pbts compiler.
+  if (options.pbts) {
+    await execPbts(context.resourcePath, options.pbts, compiledContent);
+  }
+
+  // Ensure all dependencies are marked before returning a value.
+  await loadDependencies;
+
+  return compiledContent;
+};
+
+/** @type { (this: LoaderContext, source: string) => void } */
 module.exports = function protobufJsLoader(source) {
   const callback = this.async();
-  const self = this;
 
   // Explicitly check this case, as the typescript compiler thinks
   // it's possible.
@@ -137,135 +291,11 @@ module.exports = function protobufJsLoader(source) {
     throw new Error('Failed to request async execution from webpack');
   }
 
-  try {
-    const defaultPaths = (() => {
-      if (this._compiler) {
-        // The `_compiler` property is deprecated, but still works as
-        // of webpack@5.
-        return (this._compiler.options.resolve || {}).modules;
-      }
-
-      return undefined;
-    })();
-
-    /** @type LoaderOptions */
-    const options = {
-      target: TARGET_STATIC_MODULE,
-      // Default to the module search paths given to the compiler.
-      paths: defaultPaths || [],
-      pbjsArgs: [],
-      pbts: false,
-      ...this.getOptions(),
-    };
-    validateOptions(schema, options, { name: 'protobufjs-loader' });
-
-    /** @type { string } */
-    new Promise((resolve, reject) => {
-      tmp.file((err, filename) => {
-        if (err) {
-          reject(err);
-        } else {
-          resolve(filename);
-        }
-      });
+  loadSource(source, this)
+    .then((compiled) => {
+      callback(undefined, compiled);
     })
-      .then(
-        (filename) =>
-          new Promise((resolve, reject) => {
-            fs.writeFile(filename, source, (err) => {
-              if (err) {
-                reject(err);
-              } else {
-                resolve(filename);
-              }
-            });
-          })
-      )
-      .then((filename) => {
-        const { paths } = options;
-
-        const loadDependencies = new Promise((resolve, reject) => {
-          const root = new protobuf.Root();
-          root.resolvePath = (origin, target) => {
-            // Adapted from
-            // https://github.com/dcodeIO/protobuf.js/blob/master/cli/pbjs.js
-            const normOrigin = protobuf.util.path.normalize(origin);
-            const normTarget = protobuf.util.path.normalize(target);
-
-            let resolved = protobuf.util.path.resolve(
-              normOrigin,
-              normTarget,
-              true
-            );
-            const idx = resolved.lastIndexOf('google/protobuf/');
-            if (idx > -1) {
-              const altname = resolved.substring(idx);
-              if (altname in protobuf.common) {
-                resolved = altname;
-              }
-            }
-
-            if (fs.existsSync(resolved)) {
-              // Don't add a dependency on the temp file
-              if (resolved !== protobuf.util.path.normalize(filename)) {
-                self.addDependency(resolved);
-              }
-              return resolved;
-            }
-
-            for (let i = 0; i < paths.length; i += 1) {
-              const iresolved = protobuf.util.path.resolve(
-                `${paths[i]}/`,
-                target
-              );
-              if (fs.existsSync(iresolved)) {
-                self.addDependency(iresolved);
-                return iresolved;
-              }
-            }
-
-            return null;
-          };
-          protobuf.load(filename, root, (err, result) => {
-            if (err) {
-              reject(err);
-            } else {
-              resolve(result);
-            }
-          });
-        });
-
-        /** @type { string[] } */
-        let args = ['-t', options.target];
-        paths.forEach((path) => {
-          args = args.concat(['-p', path]);
-        });
-        args = args.concat(options.pbjsArgs).concat([filename]);
-
-        pbjs.main(args, (err, result) => {
-          // Make sure we've added all dependencies before completing.
-          loadDependencies
-            .catch((depErr) => {
-              callback(depErr);
-            })
-            .then(() => {
-              if (!options.pbts || err) {
-                callback(err, result);
-              } else {
-                execPbts(
-                  self.resourcePath,
-                  options.pbts,
-                  result || '',
-                  callback
-                );
-              }
-            });
-        });
-      })
-      .catch((err) => {
-        callback(err instanceof Error ? err : new Error(`${err}`), undefined);
-      });
-  } catch (err) {
-    callback(err instanceof Error ? err : new Error(`${err}`), undefined);
-  }
+    .catch((err) => {
+      callback(err, undefined);
+    });
 };
